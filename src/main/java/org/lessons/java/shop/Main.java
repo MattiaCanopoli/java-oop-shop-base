c0f9673194d920316995a74405d4ee80c244d830
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-06-12 00:36:15 +0200

    fix: method type and return; feat: min code number; added "AndCurrency" methods
    changed method type from void to int (price/vatPrice) and from void to String (fullName). added return
    
    added two new methods returning a String of price/vatPrice + €

--- src/main/java/org/lessons/java/shop/Main.java
+++ src/main/java/org/lessons/java/shop/Main.java
@@ -5,17 +5,25 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Prodotto prodotto1 = new Prodotto("name", "description", 100, 22);
+		Prodotto prodotto1 = new Prodotto("prod1", "descr1", 800, 22);
 
-		prodotto1.showPrice();
-		prodotto1.vatPrice();
-		prodotto1.fullName();
+		System.out.println(prodotto1.productPrice);
+		System.out.println(prodotto1.price());
+		System.out.println(prodotto1.priceAndCurrency());
+		System.out.println(prodotto1.vatPrice());
+		System.out.println(prodotto1.vatPriceAndCurrency());
+		System.out.println(prodotto1.fullName());
+		System.out.println("--------------------");
 
-		Prodotto prodotto2 = new Prodotto("name", "description", 100, 22);
+		Prodotto prodotto2 = new Prodotto("prod2", "descr2", 128, 22);
 
-		prodotto2.showPrice();
-		prodotto2.vatPrice();
-		prodotto2.fullName();
+		System.out.println(prodotto2.productPrice);
+		System.out.println(prodotto2.price());
+		System.out.println(prodotto2.priceAndCurrency());
+		System.out.println(prodotto2.vatPrice());
+		System.out.println(prodotto2.vatPriceAndCurrency());
+		System.out.println(prodotto2.fullName());
+		System.out.println("--------------------");
 
 	}
 
--- src/main/java/org/lessons/java/shop/Prodotto.java
+++ src/main/java/org/lessons/java/shop/Prodotto.java
@@ -4,32 +4,49 @@ import java.util.Random;
 
 public class Prodotto {
 
-	Random code = new Random();
-	int productCode = code.nextInt(99999);;
+//	Random randomCode = new Random();
+//	int productCode = randomCode.nextInt(99999);;
+	int productCode;
 	String productName;
 	String productDescription;
 	int productPrice;
 	int vat;
 
 	public Prodotto(String productName, String productDescription, int productPrice, int vat) {
-//		this.productCode=productCode;
+
+		int minValue = 111111;
+		int maxValue = 999999;
+		Random randomCode = new Random();
+
+		this.productCode = randomCode.nextInt(maxValue - minValue) + minValue;
 		this.productName = productName;
 		this.productDescription = productDescription;
 		this.productPrice = productPrice;
 		this.vat = vat;
 	}
 
-	public void showPrice() {
-		System.out.println(productPrice + "€");
+	public int price() {
+		return productPrice;
 	}
 
-	public void vatPrice() {
+	public String priceAndCurrency() {
+		String priceAndCurrency = productPrice + "€";
+		return priceAndCurrency;
+	}
+
+	public int vatPrice() {
 		int vatPrice = productPrice + ((productPrice * vat) / 100);
-		System.out.println(vatPrice + "€");
+		return vatPrice;
 	}
 
-	public void fullName() {
-		System.out.println(productCode + productName);
+	public String vatPriceAndCurrency() {
+		String vatPrice = (productPrice + ((productPrice * vat) / 100)) + "€";
+		return vatPrice;
+	}
+
+	public String fullName() {
+		String fullNameString = productCode + "-" + productName;
+		return fullNameString;
 	}
 
 }
