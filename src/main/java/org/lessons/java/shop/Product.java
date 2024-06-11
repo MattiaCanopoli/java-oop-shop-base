4b4e80bfdc5e3ef222517a5ccba222c45781acba
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-06-11 22:26:54 +0200

    Add: Product class

---
+++ src/main/java/org/lessons/java/shop/Product.java
@@ -0,0 +1,24 @@
+package org.lessons.java.shop;
+
+public class Product {
+	
+	int productCode;
+	String productName;
+	String productDescription;
+	int productPrice;
+	int vat;
+	
+	
+	public Product (int productCode, String productName,String productDescription,int productPrice, int vat) {
+		this.productCode=productCode;
+		this.productName=productName;
+		this.productDescription=productDescription;
+		this.productPrice=productPrice;
+		this.vat=vat;
+
+	}
+	
+
+}
+
+
